16f8c2e4e5fe80bfe694ee09959f2fc36a094b17
jian10au <jian10au@gmail.com>
2019-09-28 16:17:36 +1000

    create functon works

---
+++ require.js
@@ -0,0 +1,13 @@
+// the page allow people to add the a post and username to a web page.
+// it allows people to edit its own conetent.
+// it allows a person to delete its own content.
+// it show the latest update Time.
+
+
+const d = new Date();
+
+let dateStr = `${d.getHours()}:${d.getMinutes()} ${d.getMonth()+1}/${d.getFullYear()} AEDT`
+
+// console.log(dateStr);
+
+console.log(dateStr);
